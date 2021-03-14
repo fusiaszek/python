73becd65670738c690b5c8ee2c31a287c0eebccc
fusiaszek <fusownia@gmail.com>
2021-03-14 19:32:21 +0100

    zadanie domowe ucinanie listy

--- algorytmy/zadanie ucinanie listy.py
+++ algorytmy/zadanie ucinanie listy.py
@@ -1,7 +1,7 @@
-def halo(lista,liczba):
+def usun_wszystko_przed(lista, liczba):
     u=[]
     for i in lista:
         if i>=liczba:
             u.append(i)
     print(u)
-halo([7,9,2,3,1,4],5)
+usun_wszystko_przed([7, 9, 2, 3, 1, 4], 5)
